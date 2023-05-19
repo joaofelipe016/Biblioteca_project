c180e2251656ae0d45a4232933a86790ab77b4d7
joaofelipe016 <61212164+joaofelipe016@users.noreply.github.com>
2023-05-19 19:16:32 -0300

    tentando aprimorar pessoaFisicaBuilder

--- src/main/java/com/projeto/biblioteca/services/PessoaFisicaService.java
+++ src/main/java/com/projeto/biblioteca/services/PessoaFisicaService.java
@@ -1,7 +1,8 @@
 package com.projeto.biblioteca.services;
 
-import com.projeto.biblioteca.builders.PessoaFisicaBuilder;
+import com.projeto.biblioteca.model.Bairro;
 import com.projeto.biblioteca.model.PessoaFisica;
+import com.projeto.biblioteca.repositories.BairroRepository;
 import com.projeto.biblioteca.repositories.PessoaFisicaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,24 +13,31 @@ public class PessoaFisicaService {
     @Autowired
     private PessoaFisicaRepository pessoaFisicaRepository;
 
+    @Autowired
+    private BairroRepository bairroRepository;
+
     public PessoaFisica salvar(PessoaFisica pessoaFisica) {
         return this.pessoaFisicaRepository.save(pessoaFisica);
     }
 
-    public PessoaFisica atualizar(PessoaFisica pessoaFisicaAtualizada, Long id) {
+    public PessoaFisica atualizar(PessoaFisica pessoaFisicaUpdate, Long id) {
         PessoaFisica pessoaFisica = pessoaFisicaRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("PessoaFisica não encontrada com id " + id));
-        PessoaFisica pessoaFisicaAtualizadaComId = new PessoaFisicaBuilder()
-                .withNrCpf(pessoaFisicaAtualizada.getNrCpf())
-                .withDtNascimento(pessoaFisicaAtualizada.getDtNascimento())
-                .withNmPessoa(pessoaFisica.getPessoa().getNmPessoa())
-                .withNmEmail(pessoaFisica.getPessoa().getNmEmail())
-                .withNrTelefone(pessoaFisica.getPessoa().getNrTelefone())
-                .withNrCep(pessoaFisica.getPessoa().getEndereco().getNrCep())
-                .withNmLogradouro(pessoaFisica.getPessoa().getEndereco().getNmLogradouro())
-                .withNrLogradouro(pessoaFisica.getPessoa().getEndereco().getNrLogradouro())
-                .withIdBairro(pessoaFisica.getPessoa().getEndereco().getBairro().getIdBairro())
-                .build();
-        return pessoaFisicaRepository.save(pessoaFisicaAtualizadaComId);
+        return builderPessoaFisica(pessoaFisicaUpdate, pessoaFisica);
+    }
+
+    private PessoaFisica builderPessoaFisica(PessoaFisica pessoaFisicaUpdate, PessoaFisica pessoaFisica) {
+        pessoaFisica.setNrCpf(pessoaFisicaUpdate.getNrCpf());
+        pessoaFisica.setDtNascimento(pessoaFisicaUpdate.getDtNascimento());
+        pessoaFisica.getPessoa().setNmPessoa(pessoaFisicaUpdate.getPessoa().getNmPessoa());
+        pessoaFisica.getPessoa().setNmEmail(pessoaFisicaUpdate.getPessoa().getNmEmail());
+        pessoaFisica.getPessoa().setNrTelefone(pessoaFisicaUpdate.getPessoa().getNrTelefone());
+        pessoaFisica.getPessoa().getEndereco().setNrCep(pessoaFisicaUpdate.getPessoa().getEndereco().getNrCep());
+        pessoaFisica.getPessoa().getEndereco().setNmLogradouro(pessoaFisicaUpdate.getPessoa().getEndereco().getNmLogradouro());
+        pessoaFisica.getPessoa().getEndereco().setNrLogradouro(pessoaFisicaUpdate.getPessoa().getEndereco().getNrLogradouro());
+        Bairro bairro = bairroRepository.findById(pessoaFisicaUpdate.getPessoa().getEndereco().getBairro().getIdBairro())
+                .orElseThrow(() -> new RuntimeException("Bairro não encontrado com id " + pessoaFisicaUpdate.getPessoa().getEndereco().getBairro().getIdBairro()));
+        pessoaFisica.getPessoa().getEndereco().setBairro(bairro);
+        return pessoaFisicaUpdate;
     }
 }
